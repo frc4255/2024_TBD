fea5d51758ca86710643e4313886905725195a28
RIGGZHyper <radmehrtp@gmail.com>
2024-02-08 15:41:27 -0800

    Finish alpha code of FlyWheel.java
    modified startFlyWheel method in FlyWheel.java
    deleted configureFlyWheel method in FlyWheel.java
    
    IMPORTANT!!
    Constants are still needed.
    There are no commands currently using FlyWheel.java

--- src/main/java/frc/robot/subsystems/shooter/FlyWheel.java
+++ src/main/java/frc/robot/subsystems/shooter/FlyWheel.java
@@ -6,7 +6,8 @@ import frc.robot.subsystems.shooter.*;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class FlyWheel {
-    PIDController m_PIDController = new PIDController(ShooterConstants.FLYWHEEL_P, 0, 0);
+    PIDController m_RightPIDController = new PIDController(ShooterConstants.FLYWHEEL_P, 0, 0);
+    PIDController m_LeftPIDController = new PIDController(ShooterConstants.FLYWHEEL_P, 0, 0);
 
     //TODO: Name left side right sid ebased off facing some direction
     private final TalonFX m_FlyWheelMotor0 = new TalonFX(Constants.FlyWheel.MOTOR_ID_0);
@@ -17,26 +18,26 @@ public class FlyWheel {
     public FlyWheel() {
 
     }
-    public void ConfigurePID() {
-        m_PIDController.setSetpoint(m_FlyWheelMotor0.getVelocity().getValueAsDouble() * 60);
-        m_PIDController.setTolerance(100);
-
-    }
-
     public void startFlyWheel() {
-        m_FlyWheelMotor0.setVoltage((m_FlyWheelMotor0.getVelocity().getValueAsDouble()
-        /511.998046875) * 12 * 60);
-        m_FlyWheelMotor1.setVoltage((m_FlyWheelMotor1.getVelocity().getValueAsDouble()
-        /511.998046875) * 12 * 60);
+        m_RightPIDController.calculate(0, 6000);
+        m_LeftPIDController.calculate(0, 4000);
         
-        m_PIDController.setSetpoint(m_FlyWheelMotor0.getVelocity().getValueAsDouble() * 60);
+        m_FlyWheelMotor0.setVoltage((m_LeftPIDController.getSetpoint() / 511.998046875) * 12);
+        m_FlyWheelMotor1.setVoltage((m_RightPIDController.getSetpoint() / 511.998046875) * 12);
 
-        if (m_FlyWheelMotor0.getMotorVoltage().getValueAsDouble() != 0.0 || m_FlyWheelMotor1.getMotorVoltage().getValueAsDouble() != 0.0) {
+        if (m_FlyWheelMotor0.getMotorVoltage().getValueAsDouble() != 0.0 || 
+        m_FlyWheelMotor1.getMotorVoltage().getValueAsDouble() != 0.0) {
             isRunning = true;
         }
+    }
 
-        if (m_PIDController.getSetpoint() ) {
+    public void runFlyWheel() {
+        m_FlyWheelMotor0.setVoltage((m_LeftPIDController.getSetpoint() / 511.998046875) * 12);
+        m_FlyWheelMotor1.setVoltage((m_RightPIDController.getSetpoint() / 511.998046875) * 12);
 
+        if (m_FlyWheelMotor0.getMotorVoltage().getValueAsDouble() != 0.0 ||
+         m_FlyWheelMotor1.getMotorVoltage().getValueAsDouble() != 0.0) {
+            isRunning = true;
         }
     }
 }
