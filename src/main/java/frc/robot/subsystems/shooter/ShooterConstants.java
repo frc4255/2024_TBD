8545712ae7a267dc34492b104992a7a3a8c591f9
CarlosONava <cnainca@gmail.com>
2024-02-19 17:00:26 -0800

    Update Color Sensor to use matchClosestColor()

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -11,5 +11,12 @@ public class ShooterConstants {
     public static final Map<Double, Double> LOOKUP_TABLE = Map.of();
 
     public static final double FLYWHEEL_P = 0; //TODO
-    
+
+    public static final class Hopper {
+        public static final int MOTOR_ID_0 = 0;
+        public static final int MOTOR_ID_1 = 1; 
+        public static final int ENCODER_ID = 2;
+        public static final double HOPPER_CURRENT_THRESHOLD_M0 = 0; // TODO: assign current threshold for motor 0 (star)
+        public static final double HOPPER_CURRENT_THRESHOLD_M1 = 0; // TODO: assign current threshold for motor 1 (not the star)
+    }
 }
