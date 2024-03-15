a315ff10169a00b25d97d5072dc0e366a4181348
Nicolas Fisher <nicofisher2007@gmail.com>
2024-03-14 22:52:49 -0700

    Clean up Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,40 +10,55 @@ import frc.robot.Constants.Intake.Setpoints;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 
 import java.lang.Math;
 import java.util.function.Supplier;
 
 public class Intake extends ProfiledPIDSubsystem {
-    private final TalonFX m_IntakeArmMotor = new TalonFX(Constants.Intake.MOTOR_ID_0);
+
+    /* Motors */
+    private final TalonFX m_IntakePivotMotor = new TalonFX(Constants.Intake.MOTOR_ID_0);
     private final TalonFX m_IntakeMotor = new TalonFX(Constants.Intake.MOTOR_ID_1);
 
-    private boolean m_isHomed = false;
-    private boolean m_isRunning = false;
+    /* Intake Modes */
+    private boolean isHomed = false;
+    private boolean isRunning = false;
     private boolean ampMode = false;
 
     private VoltageOut m_armJointRequest = new VoltageOut(0.0);
     private DutyCycleOut m_intakeRequest = new DutyCycleOut(0.0);
 
+    /* Suppliers */
     private Supplier<Boolean> m_CollisionAvoidanceSupplier;
 
     public Intake(Supplier<Boolean> m_ShouldMoveIntake) {
+
+        /* TODO: Tune for tensioned chain */
         super(new ProfiledPIDController(
             Constants.Intake.P, 
             0,
             0,
-            new TrapezoidProfile.Constraints(9, 10)) //TODO: Tune
+            new TrapezoidProfile.Constraints(9, 10))
         );
+
         getController().setTolerance(0.03);
         this.m_CollisionAvoidanceSupplier = m_ShouldMoveIntake;
     }
 
+    @Override
+    protected double getMeasurement() {
+        return getPivotPosition();
+    }
+
+    @Override
+    protected void useOutput(double output, TrapezoidProfile.State setpoint) {
+        m_IntakePivotMotor.setControl(m_armJointRequest.withOutput(output));
+    }
 
     public void requestGoal(Setpoints DesiredPosition) {
 
-        if (!m_isHomed) {
+        if (!isHomed) {
             DriverStation.reportWarning(
                 "WARNING: INTAKE GOAL WAS REQUESTED WHILE INTAKE IS NOT HOMED.",
                 false
@@ -53,100 +68,68 @@ public class Intake extends ProfiledPIDSubsystem {
 
         switch (DesiredPosition) {
             case DEPLOY:
-                m_isRunning = true;
+                isRunning = true;
                 ampMode = false;
                 break;
             case STOW:
-                m_isRunning = false;
+                isRunning = false;
                 ampMode = false;
                 break;
             case AMP:
                 ampMode = true;
-                m_isRunning = false;
+                isRunning = false;
                 break;
             default:
-                m_isRunning = false;
+                isRunning = false;
                 ampMode = false;
         }
-    }
-
-    @Override
-    protected double getMeasurement() {
-        return getArmPosition();
-    }
-
-    @Override
-    protected void useOutput(double output, TrapezoidProfile.State setpoint) {
-        m_IntakeArmMotor.setControl(m_armJointRequest.withOutput(output));
-        SmartDashboard.putNumber("PID Controller Out", output);
-    }
-
-    public boolean isHomed() {
-        return m_isHomed;
     }
 
     public void runIntake() {
         m_IntakeMotor.setControl(m_intakeRequest.withOutput(-0.7));
     }
 
-    public void InverserunIntake() {
+    /* TODO: Tune AMP mode intake speed. */
+    public void runIntakeForAmp() {
         m_IntakeMotor.setControl(m_intakeRequest.withOutput(0.6));
     }
+
     public void stopIntake() {
         m_IntakeMotor.stopMotor();
     }
 
     /* Used for physical button on robot */
     public void setIntakeAsHomed() {
-        m_IntakeArmMotor.setPosition(0.0);
-        m_isHomed = true;
+        m_IntakePivotMotor.setPosition(0.0);
+        isHomed = true;
     }
 
-    /* Used to set a new home position on the fly */
-
-    public void startHomeSequence() {
-        m_isHomed = false;
-        m_IntakeArmMotor.setControl(m_armJointRequest.withOutput(1.5));
+    public boolean isHomed() {
+        return isHomed;
     }
 
-    public double getArmMotorCurrent() {
-        return m_IntakeArmMotor.getStatorCurrent().getValueAsDouble();
-    }
-
-    // elbow gear ratio is 86.02:1
-    public double getArmPosition() {
-        return ((m_IntakeArmMotor.getPosition().getValueAsDouble()) / 86.02)*(2*Math.PI);
+    /* Pivot gear ratio is 86.02:1 */
+    public double getPivotPosition() {
+        return ((m_IntakePivotMotor.getPosition().getValueAsDouble()) / 86.02)*(2*Math.PI);
     }
     
     @Override
     public void periodic() {
         super.periodic();
-        /*SmartDashboard.putNumber("Arm joint position", getArmPosition());
-        SmartDashboard.putNumber("PID Error", super.getController().getPositionError());
-        SmartDashboard.putNumber("Intake motor out", m_IntakeMotor.getMotorVoltage().getValueAsDouble());
-        SmartDashboard.putNumber("Intake stator current", m_IntakeMotor.getStatorCurrent().getValueAsDouble());
 
-        SmartDashboard.putNumber("Motor voltage", m_IntakeArmMotor.getMotorVoltage().getValueAsDouble());
-        SmartDashboard.putNumber("Error", super.getController().getPositionError());
-        SmartDashboard.putNumber("Motor supply voltage", m_IntakeArmMotor.getSupplyVoltage().getValueAsDouble());
-        SmartDashboard.putNumber("Arm joint position", getArmPosition());*/
+        if (!isHomed) {
+            return;
+        }
 
-
-        SmartDashboard.putBoolean("Collision Avoidance", m_CollisionAvoidanceSupplier.get());
         if (m_CollisionAvoidanceSupplier.get()) {
             m_enabled = true;
             setGoal(2.0);
-            //setGoal(Constants.Intake.intakeSetpoints.get(Setpoints.OUT_OF_WAY));
-        } else if (m_isRunning) {
+        } else if (isRunning) {
             setGoal(Constants.Intake.intakeSetpoints.get(Setpoints.DEPLOY));
         } else if (ampMode) {
-            System.out.println("should be working");
-            setGoal(2);
+            setGoal(2); //TODO: Tune intake AMP mode setpoint
         } else {
-            System.out.println("I should not be here");
             setGoal(Constants.Intake.intakeSetpoints.get(Setpoints.STOW));
         }
-
-        SmartDashboard.putNumber("Intake motor current", m_IntakeMotor.getTorqueCurrent().getValueAsDouble());
     }
 }
